62f152c2778b59a0d271ed678525b0ff12a97d81
Axat <88314463+axatsa@users.noreply.github.com>
2025-12-01 20:48:05 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@
 
 const CONFIG = {
     // @BotFather dan olingan token
-    TELEGRAM_BOT_TOKEN: '8299184036:AAER4vaBdGJaIZBd9MdpsRyvqWI--3aoVZA',
+    TELEGRAM_BOT_TOKEN: '',
 
     // Guruh yoki kanal ID (masalan: -1001234567890)
-    TELEGRAM_CHAT_ID: '-1003244818326',
+    TELEGRAM_CHAT_ID: '-',
 
     // Standart til (uz, ru, en)
     DEFAULT_LANGUAGE: 'uz'
