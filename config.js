936df1b17ce12e4ae7d1ababfdfa6b07204af6e8
AxatSa <salamovakhat@gmail.com>
2025-12-01 20:47:26 +0500

    finish

--- config.js
+++ config.js
@@ -5,10 +5,10 @@
 
 const CONFIG = {
     // @BotFather dan olingan token
-    TELEGRAM_BOT_TOKEN: '',
+    TELEGRAM_BOT_TOKEN: '8299184036:AAER4vaBdGJaIZBd9MdpsRyvqWI--3aoVZA',
 
     // Guruh yoki kanal ID (masalan: -1001234567890)
-    TELEGRAM_CHAT_ID: '-',
+    TELEGRAM_CHAT_ID: '-1003244818326',
 
     // Standart til (uz, ru, en)
     DEFAULT_LANGUAGE: 'uz'
